133faa70ad258c7efbced78b92b637570cf71049
Simon Cicek <Simon.Cicek@tobiidynavox.com>
2016-03-18 20:52:53 +0100

    Initial setup.

---
+++ Source/Main/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Main
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Source/Main/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Main
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
